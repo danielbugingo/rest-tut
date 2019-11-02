81e39734255af78296a6ecec04e9673f401cd970
Bugingo Daniel <danielbugingo1@gmail.com>
2019-11-02 19:46:18 +0300

    Connection to Database established

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 
+require('dotenv/config');
+
+//const dbRoute = 'mongodb+srv://danielbugingo1:dbug.256@cluster0-gtr7v.mongodb.net/test?retryWrites=true&w=majority'
+
 // ROUTES
 app.get('/', (req, res)=>{
     res.send("We are on home page")
@@ -12,9 +16,14 @@ app.get('/posts', (req, res) => {
 })
 
 //Connect To DB
-mongoose.connect('mongodb+srv://danielbugingo1:dbug.123@cluster0-kjlgw.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true }, { useUnifiedTopology: true }, ()=>{
-    console.log('connected to DB')
-})
+mongoose.connect(process.env.DB_CONNECTION, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+ })
+ .then(()=>console.log(`connection to database established`))
+ .catch(err=>console.log(`db error ${err.message}`));
 
 // How we start listening to the server
 app.listen(3000);
